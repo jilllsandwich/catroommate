42e132d182f322790dd592550e3ad816c83899bb
McKenna Flanagan <singlinmingle@mailinator.com>
2019-02-28 11:18:43 -0500

    added entire unity project

---
+++ CatRoommateClone/Assets/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Button : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] textStuff;
+
+    public void ChangeText()
+    {
+        int randNum;
+
+        randNum = Random.Range(0, textStuff.Length);
+
+        text.text = textStuff[randNum];
+    }
+
+}
