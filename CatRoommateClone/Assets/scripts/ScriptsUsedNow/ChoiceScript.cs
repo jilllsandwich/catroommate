0795b9bc44bd81bc70c59f6b16a8530cd5d4bfb2
McKenna Flanagan <singlinmingle@mailinator.com>
2019-03-26 04:32:23 -0400

    game made essentially
    worked on implementation of questions with choices, also started on elaboration that includes a new time limit mechanic

--- CatRoommateClone/Assets/scripts/ScriptsUsedNow/ChoiceScript.cs
+++ CatRoommateClone/Assets/scripts/ScriptsUsedNow/ChoiceScript.cs
@@ -6,32 +6,7 @@ using UnityEngine.UI;
 
 public class ChoiceScript : MonoBehaviour
 {
-    public GameObject QuestionText;
-    public GameObject Choice01;
-    public GameObject Choice02;
-    public int ChoiceMade;
-
-    public void ChoiceOption1()
-    {
-        QuestionText.GetComponent<TextMeshProUGUI>().text = "Are you a night owl or an early riser?";
-        ChoiceMade = 1;
-
-    }
-    
-    public void ChoiceOption2()
-    {
-        QuestionText.GetComponent<TextMeshProUGUI>().text = "Are you a night owl or an early riser?";
-        ChoiceMade = 2;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (ChoiceMade >= 1)
-        {
-            Choice01.SetActive(false);
-            Choice02.SetActive(false);
-        }
-    }
+    public string QuestionText;
+    public string Choice01;
+    public string Choice02;
 }
